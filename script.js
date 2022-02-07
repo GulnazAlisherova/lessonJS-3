d68c210232bde1a292ffb0f5d1c172900eceb288
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-07 14:14:45 +0600

    added textContent

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-document.body.innerHTML = "<h1>Hello world </h1>"; // create
-let h1Element = documnet.querySelecto(`h1`); //find;
-h1Element.style.color = "rgb(255, 0 , 0)"; //modify
+// //method 1
+// document.body.innerHTML = "<h1>Hello world </h1>"; // create
+// let h1Element = documnet.querySelecto(`h1`); //find;
+// h1Element.style.color = "rgb(255, 0 , 0)"; //modify
 
-//Method 2
+// //Method 2
 
-//create
-let h2Element = document.createElement(`h2`);
-//attach
-document.body.prepend(h2Element);
-//modify
-h2Element.textContent = "Nice to meet you!";
+// //create
+// let h2Element = document.createElement(`h2`);
+// //attach
+// document.body.prepend(h2Element);
+// //modify
+// h2Element.textContent = "Nice to meet you!";
