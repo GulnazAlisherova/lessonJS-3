b28b9c93def9061395a1058db8c68b45c33a3185
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-07 14:21:53 +0600

    added h2Element

--- script.js
+++ script.js
@@ -13,8 +13,12 @@
 // h2Element.textContent = "Nice to meet you!";
 
 
-let exercisesElement = document.querySelector(`#exercises`);
+let exercisesElement = document.querySelector('#exercises');
 
-let h1Element = document.createElement(`h1`); //create
+let h1Element = document.createElement('h1'); //create
 h1Element.textContent = "h1 Header";//modify
 exercisesElement.append(h1Element); //attach
+
+let h2Element = document.createElement('h2');
+h2Element.textContent = "h2 Header";
+exercisesElement.append(h2Element)
