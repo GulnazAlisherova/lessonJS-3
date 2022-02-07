21c46b7d0ed016c8b592fb4f2a0788eb6f0fb9e3
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-07 14:09:57 +0600

    added h1Element.style.color

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-document.body.innerHTML = "<h1>Hello world </h1>";
+document.body.innerHTML = "<h1>Hello world </h1>"; // create
+let h1Element = documnet.querySelecto(`h1`); //find;
+h1Element.style.color = "rgb(255, 0 , 0)"; //modify
+
+//Method 2
+
+
